d52a8168e2a0b6491b5db9e8e20fa0de829f3877
Ali Budden <alisonswick@gmail.com>
2023-01-28 20:03:47 -0600

    week10

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,6 +1,11 @@
 package projects.service;
 
 import projects.entity.Project;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import projects.dao.ProjectDao;
 
 
@@ -16,6 +21,17 @@ public class ProjectService {
 	public Project addProject(Project project) {
 		return projectDao.insertProject(project);
 		
+	}
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+		
+	}
+	
+	public Project fetchProjectById(Integer projectId) {
+	Optional<Project> op = projectDao.fetchProjectById(projectId);
+	
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID =" + projectId + " does not exist."));
 	}
 
 }
